64dc5d66bde8f2aa5958fd35462c993ef6544d40
SebastianKikas <kikas@interia.pl>
2017-04-27 13:57:35 +0200

    Cross and mutation

--- src/ai_problem_komiwojazera/Chromosom.java
+++ src/ai_problem_komiwojazera/Chromosom.java
@@ -15,6 +15,11 @@ public class Chromosom {
 
     private List<Integer> places;
 
+    public Chromosom(int firstPlace) {
+        places.add(firstPlace);
+
+    }
+
     public Chromosom(List<Integer> newPlaces) {
         places = newPlaces;
     }
@@ -41,10 +46,36 @@ public class Chromosom {
 
     public Boolean isThisValue(int checkValue) {
         for (int place : places) {
-            if (checkValue == place) {
+            if (place == checkValue) {
                 return true;
             }
         }
         return false;
+    }
+
+    public void mutation(int whichPlace, int newPlace) {
+        int temp = places.get(whichPlace);
+        places.set(whichPlace, newPlace);
+        for (int i = 0; i < places.size(); i++) {
+            if ((places.get(i) == newPlace) && (i != whichPlace)) {
+                places.set(i, temp);
+                break;
+            }
+        }
+    }
+
+    public void cross(int beginChange, int endChange, List<Integer> newPlaces) {
+        int j = 0;
+        for (int i = beginChange; i <= endChange; i++) {
+            places.set(i, newPlaces.get(j));
+            j++;
+        }
+        for(int i=0;i<places.size();i++){
+            if (i<beginChange || i>endChange){
+            for(int k=0;k<newPlaces.size();k++){
+                    if(places.get(i)==newPlaces.get(k)){
+                        
+                    }
+        }
     }
 }
