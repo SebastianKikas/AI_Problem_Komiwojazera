4ba0101f2bf89c4308c5ab49e0d5f1d2a6d74bc9
SebastianKikas <kikas@interia.pl>
2017-07-25 02:08:50 +0200

    Testy Trial

---
+++ test/TestTrial.java
@@ -0,0 +1,163 @@
+
+import ai_problem_komiwojazera.Chromosome;
+import ai_problem_komiwojazera.Individual;
+import ai_problem_komiwojazera.ParametersInvalidOrTooLittleException;
+import ai_problem_komiwojazera.Trial;
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import org.junit.Test;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Sebastian Kikas
+ */
+public class TestTrial {
+
+    @Test
+    public void testFindBeterList() throws ParametersInvalidOrTooLittleException {
+        int[][] testData
+                = {{1, 2, 3, 4, 5, 6},
+                {1, 2, 3, 4, 5, 6},
+                {1, 2, 3, 4, 5, 6},
+                {1, 2, 3, 4, 5, 6},
+                {1, 2, 3, 4, 5, 6},
+                {1, 2, 3, 4, 5, 6},
+                {1, 2, 3, 4, 5, 6}};
+        Trial testTrial = new Trial();
+        for (int i = 0; i < 5; i++) {
+            testTrial.findBeterList(testData);
+        }
+        int sizeList = testTrial.getBestIndividualsList().size();
+        int sizeChromosome = testTrial.getBestIndividualsList().get(0).getChromosome().getListPlaces().size();
+        Chromosome testChromosomeOne, testChromosomeTwo;
+        Individual testIndividual;
+        Individual testBestIndividual = testTrial.getBestIndividualsList().get(0);
+        for (int i = 0; i < sizeList; i++) {
+            testChromosomeOne = testTrial.getBestIndividualsList().get(i).getChromosome();
+            for (int j = 0; j < sizeList; j++) {
+                if (i != j) {
+                    testChromosomeTwo = testTrial.getBestIndividualsList().get(j).getChromosome();
+                    int k = 0;
+                    while (testChromosomeOne.getPlace(k) == testChromosomeTwo.getPlace(k)) {
+                        k++;
+                        if (k == sizeChromosome) {
+                            fail("Te chrommosomy są takie same " + i + " " + testChromosomeOne.getListPlaces().toString()
+                                    + " " + j + " " + testChromosomeTwo.getListPlaces().toString());
+
+                        }
+                    }
+                }
+
+            }
+
+            testIndividual = testTrial.getBestIndividualsList().get(i);
+            if (testIndividual.getDistanceAll() < testBestIndividual.getDistanceAll()) {
+                fail("Piersza na liście nie ma najniższej wartości");
+            }
+            if ((i + 1) < sizeList) {
+                if (testIndividual.getDistanceAll() > testTrial.getBestIndividualsList().get(i + 1).getDistanceAll()) {
+                    fail("Kolejne wartości nie są coraz wyższe " + testIndividual.getDistanceAll() + " "
+                            + testTrial.getBestIndividualsList().get(i + 1).getDistanceAll());
+                }
+
+            }
+        }
+    }
+
+    @Test
+    public void testMakeMutation() throws ParametersInvalidOrTooLittleException {
+        int[][] testData = {{1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}};
+        Trial testTrial = new Trial();
+        testTrial.setData(testData);
+        testTrial.makeFirstList();
+        Individual testIndividual = testTrial.getBestIndividualsList().get(0);
+        Chromosome testChromosome = new Chromosome();
+        for (int i = 0; i < testTrial.getBestIndividualsList().get(0).getChromosome().getListPlaces().size(); i++) {
+            testChromosome.setNewPlace(testIndividual.getChromosome().getPlace(i));
+        }
+
+        int howManyPlaces = 6;
+        testTrial.makeMutation(testIndividual, howManyPlaces);
+
+        int k = 0;
+        for (int i = 0; i < testTrial.getBestIndividualsList().get(0).getChromosome().getListPlaces().size(); i++) {
+            if (testChromosome.getPlace(i) == testIndividual.getChromosome().getPlace(i)) {
+                k++;
+            }
+            for (int j = 0; j < testTrial.getBestIndividualsList().get(0).getChromosome().getListPlaces().size(); j++) {
+                if (i != j) {
+                    if (testTrial.getBestIndividualsList().get(0).getChromosome().getPlace(i)
+                            == testTrial.getBestIndividualsList().get(0).getChromosome().getPlace(j)) {
+                        fail("Te miejsca w chromosomie są takie same - makeMutation- " + j + " " + i);
+
+                    }
+                }
+            }
+        }
+        if (k == testTrial.getBestIndividualsList().get(0).getChromosome().getListPlaces().size()) {
+            fail("Te chrommosomy są takie same - makeMutation " + testChromosome.getListPlaces().toString() + " " + testIndividual.getChromosome().getListPlaces().toString());
+        }
+
+    }
+
+    @Test
+    public void testMakeCross() {
+        int[][] testData = {{1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}};
+        Trial testTrial = new Trial();
+        testTrial.setData(testData);
+        testTrial.makeFirstList();
+        Individual testIndividual = testTrial.getBestIndividualsList().get(0);
+        Chromosome testChromosome = new Chromosome();
+        for (int i = 0; i < testTrial.getBestIndividualsList().get(0).getChromosome().getListPlaces().size(); i++) {
+            testChromosome.setNewPlace(testIndividual.getChromosome().getPlace(i));
+        }
+        
+        
+    }
+
+    @Test
+    public void testMakeFirstList() {
+        int[][] testData = {{1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}};
+        Trial testTrial = new Trial();
+        testTrial.setData(testData);
+        testTrial.makeFirstList();
+        int sizeList = testTrial.getBestIndividualsList().size();
+        int sizeChromosome = testTrial.getBestIndividualsList().get(0).getChromosome().getListPlaces().size();
+        Chromosome testChromosomeOne, testChromosomeTwo;
+        Individual testIndividual;
+        Individual testBestIndividual = testTrial.getBestIndividualsList().get(0);
+        for (int i = 0; i < sizeList; i++) {
+            testChromosomeOne = testTrial.getBestIndividualsList().get(i).getChromosome();
+            for (int j = 0; j < sizeList; j++) {
+                if (i != j) {
+                    testChromosomeTwo = testTrial.getBestIndividualsList().get(j).getChromosome();
+                    int k = 0;
+                    while (testChromosomeOne.getPlace(k) == testChromosomeTwo.getPlace(k)) {
+                        k++;
+                        if (k == sizeChromosome) {
+                            fail("Te chrommosomy są takie same " + i + " " + j);
+                        }
+                    }
+
+                }
+            }
+            testIndividual = testTrial.getBestIndividualsList().get(i);
+            if (testIndividual.getDistanceAll() < testBestIndividual.getDistanceAll()) {
+                fail("Piersza na liście nie ma najniższej wartości");
+            }
+            if ((i + 1) < sizeList) {
+                if (testIndividual.getDistanceAll() > testTrial.getBestIndividualsList().get(i + 1).getDistanceAll()) {
+                    fail("Kolejne wartości nie są coraz wyższe " + i);
+                }
+
+            }
+        }
+    }
+}
